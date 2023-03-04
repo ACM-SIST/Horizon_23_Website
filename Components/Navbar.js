79a1d38ddb1f3c690283ac1aaba0291164961e77
Rohith Challa <rohithsince2002@gmail.com>
2023-03-04 11:14:26 +0530

    Added Hamburger into Navbar

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,15 +1,39 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { useState,useEffect, use } from 'react'
 import acmw_logo from "../public/acmw_logo.png"
 import acm_logo from "../public/acm_logo.jpeg"
 import sist_logo from "../public/sist_logo.jpeg"
+import hamburger from "../public/hamburger.png"
 import styles from '../styles/navbar.module.css'
 
 export default function Navbar(){
+
+    const [toggleMenu,setToggleMenu]=useState(false)
+
+    const togglenav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
     return (
         <div className={styles.navbar}>
+            <div className={styles.ham_container}>
+                <button onClick={togglenav}><Image src={hamburger} alt="hamburger"/></button>
+            </div>
             <div className={styles.nav_left}>
-                <Image className={styles.sist_logo} src={sist_logo} width='50' height='50' alt='SIST LOGO'/>
-                <h3 className={styles.sist_text}>SATHYABAMA INSTITUTE OF SCIENCE AND TECHNOLOGY</h3>
+               <Image className={styles.sist_logo} src={sist_logo} width='50' height='50' alt='SIST LOGO'/>
+               {(toggleMenu) ? (<div className={styles.links_container}>
+                    <Link className={styles.nav_links} href="/" alt="home">HOME</Link>
+                    <Link className={styles.nav_links} href="/" alt="home">EVENTS</Link>
+                    <Link className={styles.nav_links} href="/" alt="home">ABOUT US</Link>
+                </div>)
+                :
+                (<div className={styles.links_container_passive}>
+                    <Link className={styles.nav_links} href="/" alt="home">HOME</Link>
+                    <Link className={styles.nav_links} href="/" alt="home">EVENTS</Link>
+                    <Link className={styles.nav_links} href="/" alt="home">ABOUT US</Link>
+                </div>)
+                }
             </div>
             <div className={styles.nav_right}>
                 <Image className={styles.acm_logo} src={acm_logo} width='50' height='50' alt='ACM SIST LOGO'/>
