36a834f35e26bc86e7c7d7882ce99c8e92567a07
Rohith Challa <rohithsince2002@gmail.com>
2023-03-08 13:00:28 +0530

    initial commit

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -2,9 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useState,useEffect, use } from 'react'
 import acmw_logo from "../public/acmw_logo.png"
-import acm_logo from "../public/acm_logo.jpeg"
+import acm_logo from "../public/acm_logo.png"
 import sist_logo from "../public/sist_logo.jpeg"
-import hamburger from "../public/hamburger.png"
+import hamburger from "../public/ham_test.png"
+import ham_cross from "../public/ham_cross.png"
 import styles from '../styles/navbar.module.css'
 
 export default function Navbar(){
@@ -17,15 +18,20 @@ export default function Navbar(){
 
     return (
         <div className={styles.navbar}>
-            <div className={styles.ham_container}>
+            {(toggleMenu) ? (<div className={styles.ham_container}>
+                <button onClick={togglenav}><Image src={ham_cross} alt="hamburger"/></button>
+            </div>)
+                :
+            (<div className={styles.ham_container}>
                 <button onClick={togglenav}><Image src={hamburger} alt="hamburger"/></button>
-            </div>
+            </div>)
+            }
             <div className={styles.nav_left}>
                <Image className={styles.sist_logo} src={sist_logo} width='50' height='50' alt='SIST LOGO'/>
                {(toggleMenu) ? (<div className={styles.links_container}>
-                    <Link className={styles.nav_links} href="/" alt="home">HOME</Link>
-                    <Link className={styles.nav_links} href="/" alt="home">EVENTS</Link>
-                    <Link className={styles.nav_links} href="/" alt="home">ABOUT US</Link>
+                    <Link className={styles.nav_links} href="/" alt="home"><p>HOME</p></Link>
+                    <Link className={styles.nav_links} href="/" alt="home"><p>EVENTS</p></Link>
+                    <Link className={styles.nav_links} href="/" alt="home"><p>ABOUT US</p></Link>
                 </div>)
                 :
                 (<div className={styles.links_container_passive}>
@@ -36,9 +42,9 @@ export default function Navbar(){
                 }
             </div>
             <div className={styles.nav_right}>
-                <Image className={styles.acm_logo} src={acm_logo} width='50' height='50' alt='ACM SIST LOGO'/>
+                <Image className={styles.acm_logo} src={acm_logo} width='60' height='60' alt='ACM SIST LOGO'/>
                 <div className={styles.sep}></div>
-                <Image className={styles.acm_logo} src={acmw_logo} width='50' height='50' alt='ACM SIST LOGO'/>
+                <Image className={styles.acm_logo} src={acmw_logo} width='60' height='60' alt='ACM SIST LOGO'/>
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,93 @@
-/* eslint-disable @next/next/no-page-custom-font */
-import { useState } from 'react'
-import Head from 'next/head';
+import Image from 'next/image'
 import Navbar from '../Components/Navbar'
+import horizon from "../public/horizon.png"
+import desc_img from "../public/desc_img.avif"
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
 
-
-
-export default function Home() {
+function Index() {
   const [toggle,setToggle] = useState(1);
+  const [timerdays,settimerdays] = useState(0);
+  const [timerhours,settimerhours] = useState(0);
+  const [timerminutes,settimerminutes] = useState(0);
+  const [timerseconds,settimerseconds] = useState(0);
 
   const toggledate = (id) => {
     setToggle(id);
   }
+
+  let interval;
+
+  const start = () => {
+    const countDownDate = new Date("March 23,2023").getTime();
+    interval = setInterval(() => {
+      const now=new Date().getTime();
+      const diff=countDownDate-now;
+      const days=Math.floor(diff/(24*60*60*1000))
+      const hours=Math.floor(diff%(24*60*60*1000)/(1000*60*60))
+      const minutes=Math.floor(diff%(60*60*1000)/(1000*60))
+      const seconds=Math.floor(diff%(60*1000)/(1000))
+      if(diff<0){
+        clearInterval(interval.current)
+      }
+      else{
+         settimerdays(days);
+         settimerhours(hours);
+         settimerminutes(minutes);
+         settimerseconds(seconds);
+      }
+    })
+  }
+  useEffect(()=>{
+    start()
+  })
   return (
-    <div>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Golos+Text&family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
-      </Head>
-      <div className={styles.hero}>
-        <Navbar />
-        <div className={styles.hero_center}>
+    <>
+      <Navbar />
+      <div className={styles.hero_container}>
+        <div className={styles.hero_main}>
+          <Image className={styles.hero_heading_img} src={horizon} alt="HORIZON"/>
+          {/* <div className={styles.hero_heading_container}>
+            <h3>MARCH 23 AND 24</h3>
+          </div> */}
+          <button className={styles.register_btn}>REGISTER NOW</button>
+        </div>
+      </div>
+      <div className={styles.desc_container}>
+        <div className={styles.desc_left}>
+          <h4 className={styles.about}>ABOUT EVENT</h4>
           <h1 className={styles.heading}>HORIZON 2023</h1>
-          <h1 className={styles.second_heading}>MARCH 24</h1>
+          <p className={styles.content}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
         </div>
-        <div></div>
-      </div>
-      <div className={styles.about}>
-        <div className={styles.about_left}>
-          <h1>ABOUT</h1>
-          <h1>HORIZON</h1>
-        </div>
-        <div className={styles.about_right}>
-          <div className={styles.desc_cont}>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultrices venenatis feugiat. Ut posuere nunc sit amet porta volutpat. Donec accumsan libero in elementum condimentum. Donec placerat nibh ipsum.</h3>
-          </div>
-          <div className={styles.more_desc_cont}>
-            <div>
-              <h2>2</h2>
-              <h3>DAYS</h3>
-            </div>
-            <div>
-              <h2>10+</h2>
-              <h3>EVENTS</h3>
-            </div>
-            <div>
-              <h2>EXCITING</h2>
-              <h3>REWARDS</h3>
-            </div>
-          </div>
+        <div className={styles.desc_right}>
+          <Image className={styles.desc_img} src={desc_img} alt="horizon"/>
         </div>
       </div>
-      <div className={styles.register}><button className={styles.register_btn}>REGISTER NOW</button></div>
+      <div className={styles.count_container}>
+        <div className={styles.count_left}>
+          <h1 className={styles.count_about}>Event Date</h1>
+          <h3>MARK YOUR CALENDERS</h3>
+          <p>COUNT EVERY SECOND UNTIL THE EVENT</p>
+        </div>
+        <div className={styles.count_right}>
+          <div className={styles.count_box}>
+            <h1>{timerdays}</h1>
+            <h1>Day</h1>
+          </div>
+          <div className={styles.count_box}>
+            <h1>{timerhours}</h1>
+            <h1>Hrs</h1>
+          </div>
+          <div className={styles.count_box}>
+            <h1>{timerminutes}</h1>
+            <h1>Min</h1>
+          </div>
+          <div className={styles.count_box}>
+            <h1>{timerseconds}</h1>
+            <h1>Sec</h1>
+          </div>
+        </div>
+      </div>
       <div className={styles.schedule_container}>
         <h1 className={styles.schedule_heading}>SCHEDULE</h1>
         <div className={styles.schedule_table}>
@@ -61,13 +96,13 @@ export default function Home() {
             className={toggle === 1 ? styles.selected : ''}
             onClick={()=>toggledate(1)}
             >
-              24th Mar
+              24th Mar 2023
             </p>
             <p
             className={toggle === 2 ? styles.selected : ''}
             onClick={()=>toggledate(2)}
             >
-              25th Mar
+              25th Mar 2023
             </p>
           </div>
           <div
@@ -80,6 +115,27 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </div>
+      <div className={styles.footer}>
+        <p className={styles.footer_heading}>HAVE QUESTION?</p>
+        <h1 className={styles.footer_contact_heading}>CONTACT US</h1>
+        <div className={styles.footer_container}>
+          <div className={styles.footer_left}>
+            <p>Address:</p>
+            <h3>184 Main Collins Street</h3>
+            <p>Phone:</p>
+            <h3>(226) 446 9371</h3>
+            <p>Email:</p>
+            <h3>confer@gmail.com</h3>
+            <p>Website:</p>
+            <h3>www.confer.com</h3>
+          </div>
+          <div className={styles.footer_right}>
+            <h3>CONTACT US</h3>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
+
+export default Index
